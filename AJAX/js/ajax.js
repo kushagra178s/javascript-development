03048d044d0dee29ff6bfe7feec3180181359aaf
KSX <kushagra17800@gmail.com>
2021-12-23 18:15:08 +0530

    23/12/21

--- AJAX/js/ajax.js
+++ AJAX/js/ajax.js
@@ -1,31 +1,87 @@
-console.log("ajax");
+console.log("Ajax tutorial in one video");
 
-let fetchbtn = document.getElementById('fetchbtn');
-fetchbtn.addEventListener('click', buttonClickHandler);
 
-function buttonClickHandler(){
-    console.log("clicked the fetch button");
+let fetchBtn = document.getElementById('fetchBtn');
+fetchBtn.addEventListener('click', buttonClickHandler)
+
+function buttonClickHandler() {
+     console.log('You have clicked the fetchBtn');
+
+    // Instantiate an xhr object
     const xhr = new XMLHttpRequest();
 
-    xhr.open('GET' , 'harry.txt',true);
+    // Open the object
+    // xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos/1', true);
 
+    // USE THIS FOR POST REQUEST
+    xhr.open('POST', 'http://dummy.restapiexample.com/api/v1/create', true);
+    xhr.getResponseHeader('Content-type', 'application/json');
+
+
+    // What to do on progress (optional)
     xhr.onprogress = function(){
-        console.log("on progress");
+        console.log('On progress');
     }
 
-    xhr.onreadystatechange = function(){
-        console.log("ready state ", xhr.readyState);
-    }
 
-    xhr.onload = function(){
+    // xhr.onreadystatechange = function () {
+    //     console.log('ready state is ', xhr.readyState);
+        
+    // }
+
+    // What to do when response is ready
+    xhr.onload = function () {
         if(this.status === 200){
-            console.log(this.responseText);
+
+            console.log(this.responseText)
         }
         else{
-            console.log("some error");
+            console.log("Some error occured")
         }
-
     }
 
-    xhr.send();
+    // send the request
+    params = `{"name":"test34sad545","salary":"123","age":"23"}`;
+    xhr.send(params);
+
+    console.log("We are done!");
+
 }
+
+let popBtn = document.getElementById('popBtn');
+popBtn.addEventListener('click', popHandler);
+
+function popHandler() {
+    console.log('You have clicked the pop handler');
+
+    // Instantiate an xhr object
+    const xhr = new XMLHttpRequest();
+
+    // Open the object
+    xhr.open('GET', 'http://dummy.restapiexample.com/api/v1/employees', true);
+
+
+    // What to do when response is ready
+    xhr.onload = function () {
+        if(this.status === 200){
+            let obj = JSON.parse(this.responseText);
+            console.log(obj);
+            let list = document.getElementById('list');
+            str = "";
+            for (key in obj)
+            {
+                str += `<li>${obj[key].employee_name} </li>`;
+            }
+            list.innerHTML = str;
+        }
+        else{
+            console.log("Some error occured")
+        }
+    }
+
+    // send the request
+    xhr.send();
+    console.log("We are done fetching employees!");
+    
+}
+
--- tut34.js
+++ tut34.js
@@ -1,8 +1,13 @@
-console.log("tut34");
+console.log("This is tutorial 34");
+
 setTimeout(() => {
-    for (let i = 0; i < 45; i++) {
-        let element = i;
-        console.log(element);
+    for (let index = 0; index < 4005; index++) {
+        const element = index;
+        console.log("This is index number" + index);
+        
     }
 }, 100);
-console.log("done");
+
+
+
+console.log('done printing');
